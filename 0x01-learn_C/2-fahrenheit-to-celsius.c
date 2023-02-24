d5412c129b577aab967f18e4c9af7aaeab9dfa71
brk-a <fnjakai@outlook.com>
2023-02-24 03:52:40 +0000

    use `#define` to refactor code. Use `putchar` and `getchar`

---
+++ 0x01-learn_C/2-fahrenheit-to-celsius.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+void main() /*fahrenheit to celsisus table*/
+{
+    int fahr;
+    for (fahr=0; fahr<=300; fahr=fahr + 20)
+        printf("%4d %6.1f\n", fahr, (5.0/9.0)*(fahr - 32));
+}
