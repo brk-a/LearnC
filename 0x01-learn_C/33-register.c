5fe6869b453bb15fb66f73f321dc34d7f7ab6de0
brk-a <fnjakai@outlook.com>
2023-03-16 03:55:17 +0000

    ..

--- 0x01-learn_C/33-register.c
+++ 0x01-learn_C/33-register.c
@@ -14,7 +14,7 @@ int main(){
     printf("compute %d\n", compute);
     for(iter=0; iter<1000; iter++){
         compute = (compute * 22) / 7;
-        if(compute>1000) compute %= 1000;
+        if(compute>1000) compute = compute % 1000;
     }
     printf("compute %d\n", compute);
 }
